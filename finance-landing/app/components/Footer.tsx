cc0633d11c0232cc7844c7d8b5c60aea256f82b2
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-09-29 16:27:37 +0000

    Header e Footer

--- finance-landing/app/components/Footer.tsx
+++ finance-landing/app/components/Footer.tsx
@@ -1,15 +1,15 @@
 // components/Footer.tsx
 export default function Footer() {
     return (
-      <footer className="bg-gray-800 text-white py-4">
+      <footer className="bg-[#A8C9FF] text-white py-4">
         <div className="container mx-auto text-center">
-        <div className="flex justify-center space-x-4 mt-2">
+        <div className="flex text-black justify-center space-x-4 mt-2">
             <a href="/about" className="hover:underline">Sobre</a>
             <a href="/connection" className="hover:underline">Conexão</a>
             <a href="/stories" className="hover:underline">Contos</a>
             <a href="/team" className="hover:underline">Equipe</a>
           </div>
-          <p>&copy; {new Date().getFullYear()} Contas de Fada. Todos os direitos reservados.</p>
+          <p className="text-black">&copy; {new Date().getFullYear()} Contas de Fada. Todos os direitos reservados.</p>
         </div>
       </footer>
     );
--- finance-landing/app/components/Header.tsx
+++ finance-landing/app/components/Header.tsx
@@ -2,9 +2,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Header() {
+export default function Header() { 
   return (
-    <header className="bg-gradient-to-b from-pink-300 to-blue-400 p-4">
+    <header className="bg-gradient-to-b from-[#ECD5FE] to-[#A8C9FF] p-4">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo com Imagem */}
         <div className="flex items-center">
@@ -12,8 +12,8 @@ export default function Header() {
             <Image
               src="/images/logo_contas.png" // Caminho da imagem no diretório 'public/images'
               alt="Conto de Fadas Logo"
-              width={150} // Largura da imagem
-              height={150} // Altura da imagem
+              width={250} // Largura da imagem
+              height={250} // Altura da imagem
             />
           </Link>
         </div>
@@ -21,21 +21,21 @@ export default function Header() {
         {/* Navigation Links como botões */}
         <nav className="flex space-x-4">
           <Link href="/sobre">
-            <button className="bg-[#A8C9FF] text-white px-4 py-2 rounded-lg shadow-lg shadow-gray-500 hover:bg-blue-300 transition">
+            <button className="bg-[#A8C9FF] text-black px-4 py-2 rounded-lg shadow-md shadow-gray-500 hover:bg-blue-300 transition">
               Sobre
             </button>
           </Link>
           <Link href="/conexao">
-            <button className="bg-[#A8C9FF] text-white px-4 py-2 rounded-lg shadow-lg shadow-gray-500 hover:bg-blue-300 transition">
+            <button className="bg-[#A8C9FF] text-black px-4 py-2 rounded-lg shadow-md shadow-gray-500 hover:bg-blue-300 transition">
               Conexão
             </button>
           </Link>
           <Link href="/contos">
-            <button className="bg-[#A8C9FF] text-white px-4 py-2 rounded-lg shadow-lg shadow-gray-500 hover:bg-blue-300 transition">
+            <button className="bg-[#A8C9FF] text-black px-4 py-2 rounded-lg shadow-lg shadow-gray-500 hover:bg-blue-300 transition">
               Contos
             </button>
           </Link>
-          <button className="bg-[#A8C9FF] text-white px-4 py-2 rounded-lg shadow-lg shadow-gray-500 hover:bg-blue-300 transition">
+          <button className="bg-[#A8C9FF] text-black px-4 py-2 rounded-lg shadow-lg shadow-gray-500 hover:bg-blue-300 transition">
             Login
           </button>
         </nav>
